c168403942bd2654f3b9fb2e823ce5ba2983af15
Kira <odnoralkira432@gmail.com>
2023-07-06 20:33:17 +0300

    Class inheritance.  Method super()

---
+++ homework4.py
@@ -0,0 +1,61 @@
+# Class Inheritance
+
+class Helloworld:
+    hello = "Hello"
+    _hello = "_Hello"
+    __hello = "__Hello"
+
+    def __init__(self):
+        self.world = "World"
+        self._world = "_World"
+        self.__world = "__World"
+
+    def printer(self):
+        print(self.hello)
+        print(self._hello)
+        print(self.__hello)
+        print(self.world)
+        print(self._world)
+        print(self.__world)
+
+
+class Hi(Helloworld):
+    def hi_print(self):
+        print(self.hello)
+        print(self.world)
+        print(self._hello)
+        print(self._world)
+
+
+
+hello = Helloworld()
+hello.printer()
+hi = Hi()
+hi.hi_print()
+
+
+# method super() and multiple inheritance
+
+
+class Computer:
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.memory = 128
+
+
+class Display:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.resolution = "4k"
+
+
+class SmartPhone(Display, Computer):
+    def print_info(self):
+        print(self.model)
+        print(self.resolution)
+        print(self.memory)
+
+
+iphone = SmartPhone(model="Last")
+iphone.print_info()
